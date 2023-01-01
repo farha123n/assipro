abed7577a368c93bd8726ecf047312f19c8d0b6c
unknown <you@example.com>
2022-12-31 19:00:46 -0800

    assinment

--- src/test/java/Unirestqw.java
+++ src/test/java/Unirestqw.java
@@ -9,8 +9,8 @@ public class Unirestqw {
     @Test
     public void post_method() throws UnirestException {
         HttpResponse<JsonNode> response = Unirest.post("https://gorest.co.in/public/v2/users")
-                .header("accept", "application/json")
-                .queryString("Authorization", "Bearer 32c80880fe3786cc2f271303a83d13c186ffe28e512260b97fc4dd5c151ff64f")
+                .header("Authorization", "Bearer 32c80880fe3786cc2f271303a83d13c186ffe28e512260b97fc4dd5c151ff64f")
+
                 .field("name", "farhan")
                 .field("email", "nil26566@gmail.com")
                 .field("gender","female")
@@ -22,20 +22,30 @@ public class Unirestqw {
     }
     @Test
     public void get_method() throws UnirestException {
-        HttpResponse<JsonNode> response1 = Unirest.get("https://gorest.co.in/public/v2/users/23")
-                .header("accept", "application/json")
-                .queryString("Authorization", "Bearer 32c80880fe3786cc2f271303a83d13c186ffe28e512260b97fc4dd5c151ff64f")
+        HttpResponse<JsonNode> response1 = Unirest.get("https://gorest.co.in/public/v2/users/174")
+                .header("Authorization", "Bearer 32c80880fe3786cc2f271303a83d13c186ffe28e512260b97fc4dd5c151ff64f")
+
 
                 .asJson();
         System.out.println(response1.getStatus());
+        System.out.println(response1.getBody());
 
     }
     @Test
     public void put_method() throws UnirestException {
-        HttpResponse<JsonNode> response2 = Unirest.put("https://gorest.co.in/public/v2/users/23")
-                .header("accept", "application/json")
-                .queryString("Authorization", "Bearer 32c80880fe3786cc2f271303a83d13c186ffe28e512260b97fc4dd5c151ff64f")
+        HttpResponse<JsonNode> response2 = Unirest.put("https://gorest.co.in/public/v2/users/174")
+                .header("Authorization", "Bearer 32c80880fe3786cc2f271303a83d13c186ffe28e512260b97fc4dd5c151ff64f")
+
                 .field("name","far")
+                .asJson();
+        System.out.println(response2.getStatus());
+
+    }
+    @Test
+    public void delete_method() throws UnirestException {
+        HttpResponse<JsonNode> response2 = Unirest.delete("https://gorest.co.in/public/v2/users/174")
+                .header("Authorization", "Bearer 32c80880fe3786cc2f271303a83d13c186ffe28e512260b97fc4dd5c151ff64f")
+
                 .asJson();
         System.out.println(response2.getStatus());
 
